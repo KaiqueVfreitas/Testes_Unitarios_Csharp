3a6665ef8aa4a3ef9e3b69ebf167fbaa034a37d0
KaiqueVfreitas <kaiqueffreitasvieira@gmail.com>
2025-01-27 09:34:12 -0300

    Primeiras noções básicas do Xunit do Visual Studio. Exercicio para conhecimento da plataforma proposto pelo canal TI.

---
+++ CalculadoraTestesUnitarios/CalculadoraTestesUnitarios/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿namespace CalculadoraTestesUnitarios
+{
+    public static class Calculadora
+    {
+        public static int Somar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiplicacao(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divisao(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ CalculadoraTestesUnitarios/TestCalculadora/TesteCalculadora.cs
@@ -0,0 +1,30 @@
+﻿using CalculadoraTestesUnitarios;
+using System;
+using Xunit;
+
+namespace TestCalculadora
+{
+    public class TesteCalculadora
+    {
+        [Fact]
+        public void TesteSomar()
+        {
+            Assert.Equal(20, Calculadora.Somar(10, 10));
+        }
+        [Fact]
+        public void TesteSubtrair()
+        {
+            Assert.Equal(0, Calculadora.Subtrair(10, 10));
+        }
+        [Fact]
+        public void TesteMultiplicacao()
+        {
+            Assert.Equal(100, Calculadora.Multiplicacao(10, 10));
+        }
+        [Fact]
+        public void TesteDivisao()
+        {
+            Assert.Equal(1, Calculadora.Divisao(10, 10));
+        }
+    }
+}
